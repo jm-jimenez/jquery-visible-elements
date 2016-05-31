17596099bb5ef45994f6eaf10c28d24ae883b9ff
Jose Maria Jimenez <josemariajimenezperez@gmail.com>
2016-05-31 12:26:22 +0200

    added new options for to consider an element visible

--- jquery-visible-elements.js
+++ jquery-visible-elements.js
@@ -1,6 +1,17 @@
 $.fn.isElementInViewport = function (){
 	var bottomHeight = $(window).scrollTop() + $(window).height();
-	var elementOffset = $(this).offset().top + $(this).height();
+	var elementOffset;
+	
+	if (when === "top"){
+		elementOffset = $(this).offset().top;
+	}
+	else if (when === "mid"){
+		elementOffset = $(this).offset().top + $(this).height()/2;
+	}
+	else{
+		elementOffset = $(this).offset().top + $(this).height();
+	}
+
 	if (elementOffset <bottomHeight){
 		return true;
 	}
