cf31d6f3e6669de30f17889f383d34c9c7a9534d
Jose Maria Jimenez <josemariajimenezperez@gmail.com>
2016-07-14 17:06:08 +0200

    Added new functionality based on % of the element

--- jquery-visible-elements.js
+++ jquery-visible-elements.js
@@ -8,6 +8,15 @@ $.fn.isElementInViewport = function (when){
 	else if (when === "mid"){
 		elementOffset = $(this).offset().top + $(this).height()/2;
 	}
+	else if (typeof when != "undefined"){
+		if (when < 0){
+			when = 0;
+		}
+		else if (when >100){
+			when = 100;
+		}
+		elementOffset = $(this).offset().top + $(this).height()*(when/100);
+	}
 	else{
 		elementOffset = $(this).offset().top + $(this).height();
 	}
